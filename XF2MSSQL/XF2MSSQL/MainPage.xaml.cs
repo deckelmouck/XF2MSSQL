ad62059fe07d4e6ce4780ed96c07bfb5c365b73d
Palm, Nikolas <Palm@euro-composites.com>
2020-12-11 09:53:14 +0100

    Projektdateien hinzufügen.

---
+++ XF2MSSQL/XF2MSSQL/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XF2MSSQL
+{
+    public partial class MainPage : ContentPage
+    {
+        public string SQLreturn { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            SQLreturn = "try SQL query";
+            myLabel.Text = SQLreturn;
+
+        }
+    }
+}
